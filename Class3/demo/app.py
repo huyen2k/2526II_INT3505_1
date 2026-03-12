d08b3a95d6f2a2262b1a66776f66138ed8443954
huyen2k <huyennguyen08032005@gmail.com>
2026-03-12 16:55:18 +0700

    data wrapping

--- Class3/demo/app.py
+++ Class3/demo/app.py
@@ -3,40 +3,70 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 books = [
-    {"id": 1, "title": "Clean Code", "status": "Có sẵn"},
-    {"id": 2, "title": "Design Patterns", "status": "Có sẵn"}
+    {"id": 1, "title": "Clean Code"},
+    {"id": 2, "title": "Design Patterns"},
+    {"id": 3, "title": "Refactoring"}
 ]
 
 
-# GET danh sách sách
+# GET danh sách sách (Extensible response)
 @app.route("/api/v1/books", methods=["GET"])
 def get_books():
-    return jsonify(books)
+
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 2))
+
+    start = (page - 1) * limit
+    end = start + limit
+
+    paginated_books = books[start:end]
+
+    response = {
+        "data": paginated_books,
+        "pagination": {
+            "page": page,
+            "limit": limit,
+            "total": len(books)
+        }
+    }
+
+    return jsonify(response), 200
 
 
-# GET 1 sách
+# GET một sách
 @app.route("/api/v1/books/<int:book_id>", methods=["GET"])
 def get_book(book_id):
 
     for book in books:
         if book["id"] == book_id:
-            return jsonify(book)
 
-    return {"error": "Book not found"}, 404
+            return jsonify({
+                "data": book
+            }), 200
+
+    return jsonify({
+        "error": "Book not found"
+    }), 404
 
 
-# PATCH cập nhật trạng thái sách
-@app.route("/api/v1/books/<int:book_id>", methods=["PATCH"])
-def update_status(book_id):
+# POST thêm sách
+@app.route("/api/v1/books", methods=["POST"])
+def create_book():
 
     data = request.json
 
-    for book in books:
+    new_book = {
+        "id": len(books) + 1,
+        "title": data["title"]
+    }
 
-        if book["id"] == book_id:
+    books.append(new_book)
 
-            book["status"] = data["status"]
+    return jsonify({
+        "data": new_book,
+        "message": "Book created"
+    }), 201
 
-            return jsonify(book)
 
-    return {"error": "Book not found"}, 404
+if __name__ == "__main__":
+    app.run(debug=True)
