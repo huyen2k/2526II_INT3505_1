b8e9c20ded27e81c03f9303855099463a09dacc4
huyen2k <huyennguyen08032005@gmail.com>
2026-03-12 11:27:22 +0700

    consistency

--- Class3/demo/app.py
+++ Class3/demo/app.py
@@ -1,30 +1,73 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-books_v1 = [
-    {"id": 1, "title": "Clean Code", "author": "Robert C. Martin"}
+books = [
+    {"id": 1, "title": "Clean Code"},
+    {"id": 2, "title": "Design Patterns"}
 ]
 
-books_v2 = [
-    {
-        "id": 1,
-        "title": "Clean Code",
-        "author": "Robert C. Martin",
-        "published_year": 2008,
-        "category": "Software Engineering"
+
+# GET collection
+@app.route("/api/v1/books", methods=["GET"])
+def get_books():
+    return jsonify(books)
+
+
+# GET single resource
+@app.route("/api/v1/books/<int:book_id>", methods=["GET"])
+def get_book(book_id):
+
+    for book in books:
+        if book["id"] == book_id:
+            return jsonify(book)
+
+    return {"error": "Book not found"}, 404
+
+
+# CREATE resource
+@app.route("/api/v1/books", methods=["POST"])
+def create_book():
+
+    data = request.json
+
+    new_book = {
+        "id": len(books) + 1,
+        "title": data["title"]
     }
-]
+
+    books.append(new_book)
+
+    return jsonify(new_book), 201
 
 
-@app.route("/api/v1/books")
-def get_books_v1():
-    return jsonify(books_v1)
+# UPDATE resource
+@app.route("/api/v1/books/<int:book_id>", methods=["PUT"])
+def update_book(book_id):
+
+    data = request.json
+
+    for book in books:
+
+        if book["id"] == book_id:
+            book["title"] = data["title"]
+            return jsonify(book)
+
+    return {"error": "Book not found"}, 404
 
 
-@app.route("/api/v2/books")
-def get_books_v2():
-    return jsonify(books_v2)
+# DELETE resource
+@app.route("/api/v1/books/<int:book_id>", methods=["DELETE"])
+def delete_book(book_id):
+
+    for book in books:
+        if book["id"] == book_id:
+
+            books.remove(book)
+
+            return {"message": "deleted"}
+
+    return {"error": "Book not found"}, 404
 
 
 if __name__ == "__main__":
